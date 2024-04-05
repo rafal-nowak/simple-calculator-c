e759d90e55d3dc4da3d9e50edb79f60c95c27533
Bartek <250943@edu.p.lodz.pl>
2024-04-05 13:11:09 +0200

    added subtracting

--- main.cpp
+++ main.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
+#include <cstring>
 
 void usage(char *programName) {
     printf("Usage: %s number1 operation number\n", programName);
     printf("Available operations:\n");
     printf("\tadd\n");
+    printf("\tsub\n");
     printf("\n");
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
 
     int number1 = 0;
     int number2 = 0;
@@ -21,7 +23,15 @@ int main(int argc, char* argv[]) {
             result = number1 + number2;
             printf("%d + %d = %d\n", number1, number2, result);
             return 0;
+
         }
+
+        if (!strcmp("sub", argv[2])) {
+            result = number1 - number2;
+            printf("%d - %d = %d\n", number1, number2, result);
+            return 0;
+        }
+
     }
 
     usage(argv[0]);
